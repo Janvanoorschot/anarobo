a1a0f7e8d5372ae3cbf4cde13234001760b0716c
Jan van Oorschot <janvanoorschot@gmail.com>
2019-02-10 17:08:36 +0100

    working plot

--- ana/anal101.py
+++ ana/anal101.py
@@ -1,9 +1,16 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 def loaddata():
     print("i am")
 
 
 def plotdata():
-    print("here")
+    plt.interactive(True)
+    plt.close('all')
+    ts = pd.Series(np.random.randn(1000), index=pd.date_range('1/1/2000', periods=1000))
+    df = pd.DataFrame(np.random.randn(1000, 4), index=ts.index, columns=list('ABCD'))
+    df = df.cumsum()
+    plt.figure()
+    df.plot()
