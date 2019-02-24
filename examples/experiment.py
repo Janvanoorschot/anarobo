9d0ae5df3cfeb5ac5b4369ab242503834a5ffe07
jan <janvanoorschot@gmail.com>
2019-02-24 16:08:59 +0100

    Working on plts.

--- examples/experiment.py
+++ examples/experiment.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+plt.interactive(True)
+
 dataset = pd.read_csv("basis1.gz")
 
 slis = dataset.groupby(['storyline']).describe()
@@ -42,4 +44,20 @@ dataset['score'] = pd.Series(scores)
 
 # calculate overal scores for each person
 scoresbasis1 = dataset[['person','score']].groupby(['person']).describe()
-scoresbasis1_relevant = scoresbasis1[scoresbasis1['score']['count'] > 10]
+
+# get the good/bad/weird_guys
+bad_guys = scoresbasis1[(scoresbasis1['score']['count'] >= 43) & (scoresbasis1['score']['mean'] < 1.8)]
+good_guys = scoresbasis1[(scoresbasis1['score']['count'] >= 43) & (scoresbasis1['score']['mean'] > 2.4) & (scoresbasis1['score']['mean'] != 3.0)]
+weird_guys = scoresbasis1[(scoresbasis1['score']['count'] >= 43) & (scoresbasis1['score']['mean'] == 3.0)]
+
+# start with the plotting
+x = np.linspace(0, 2, 100)
+plt.plot(x, x, label='linear')
+plt.plot(x, x**2, label='quadratic')
+plt.plot(x, x**3, label='cubic')
+plt.xlabel('x label')
+plt.ylabel('y label')
+plt.title("Simple Plot")
+plt.legend()
+plt.show()
+
