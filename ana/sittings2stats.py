ebb27db42b910d5eb801725c9debc470f5b34df1
Jan van Oorschot <janvanoorschot@gmail.com>
2019-12-15 16:54:42 +0100

    Working on stats.

--- ana/sittings2stats.py
+++ ana/sittings2stats.py
@@ -1,53 +1,32 @@
 import ana
+from datetime import date
+from datetime import datetime
 import os
 
 def sli_stats(dir):
     """ Calculate the Storyline-items (sli) statistics given the sitting-data calculated
-    from the Robomind Academy. The sli statistics consist of storyline/person/count/cumtime
-    vectors. Count is the number of time the person executed a program for the storyline
-    and cumtime is the accumulated time spend on that solution.
+    from the Robomind Academy. The calculated sli statistics consist of
+    the buckets:
+      . by weeknumber, per year c
+      . by day of the week, per year
+      . by hour of the day, per year
+    In each bucket the following counts are maintained:
+      . all
+      . ano student
+      . ident classless student
+      . class-student
+      . teacher
     """
     walker = ana.SittingWalker(dir)
     dao = ana.RoboDAO(dir)
     stats = {}
 
-    def addtime(sli_id, person, cumtime, isdone):
-        sli = dao.get_by_id('StorylineItem',sli_id)
-        sli_name = sli.fullname()
-        if sli_name not in stats:
-            entry = {}
-            stats[sli_name] = entry
-        else:
-            entry = stats[sli_name]
-        if person not in entry:
-            record = {'cumtime': 0, 'count': 0, 'completed': False, 'person': person}
-            entry[person] = record
-        else:
-            record = entry[person]
-        if not record['completed']:
-            record['count'] = record['count'] + 1
-            record['cumtime'] = record['cumtime'] + cumtime
-            if isdone:
-                record['completed'] = True
-
     for sitting in walker:
         donetoffset=0
         sli = None
         person = sitting['person']
-        for action in sitting['actions']:
-            if action['type'] == 'runscript':
-                sli = action['storylineitem']
-                if 'success' in action['details'] and action['details']['success']:
-                    isdone = True
-                else:
-                    isdone = False
-                cumtime = action['toffset'] - donetoffset
-                # there are 'cumtimes' that are negative, due to invalid timestamps, ignore those
-                if cumtime >= 0:
-                    addtime(sli, person, cumtime, isdone)
-                else:
-                    print("eikel")
-                donetoffset = action['toffset']
+        stamp = datetime.strptime(sitting['starttime'], "%Y-%m-%dT%H:%M:%SZ")
+        print(f"here {stamp}")
 
     pandastats = {}
     for sli in stats:
@@ -94,16 +73,3 @@ def generate_one_csv(np, handle, arr):
 def generate_csv(np, dirname):
     """create compressed csv files for all sli-statistics"""
     import gzip
-    filename = os.path.join(dirname, "perf_teaser.gz")
-    with gzip.open(filename, 'wt') as handle:
-        generate_one_csv(np, handle, STORYLINES_TEASER)
-    filename = os.path.join(dirname, "perf_hourofcode.gz")
-    with gzip.open(filename, 'wt') as handle:
-        generate_one_csv(np, handle, STORYLINES_HOUROFCODE)
-    filename = os.path.join(dirname, "perf_basis1.gz")
-    with gzip.open(filename, 'wt') as handle:
-        generate_one_csv(np, handle, STORYLINES_BASIS1)
-    filename = os.path.join(dirname, "perf_basis2.gz")
-    with gzip.open(filename, 'wt') as handle:
-        generate_one_csv(np, handle, STORYLINES_BASIS2)
-
--- bin/calc_stats.py
+++ bin/calc_stats.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 #
 
-from ana.sittings2perf import sli_perf, generate_csv
+from ana.sittings2stats import sli_stats, generate_csv
 SITTINGSDIR = '/data/robodata/ano'
 CVSDIR='/data/robodata/aggr'
 
 if __name__ == "__main__":
     # execute only if run as a script
     import os.path
-    np = sli_perf(SITTINGSDIR)
+    np = sli_stats(SITTINGSDIR)
     generate_csv(np, CVSDIR)
